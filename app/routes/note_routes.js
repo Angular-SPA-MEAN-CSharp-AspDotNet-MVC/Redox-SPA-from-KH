8129f94e1e2a884458564d97a04c5b72385ee76f
K. H <knhg2013@gmail.com>
2019-05-12 00:26:56 -0400

    1)Build up initial msg and source two APIs, where the data come from the sqLite.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,45 @@
+const sqlite3 = require("sqlite3").verbose();
+
 module.exports = function(app, db) {
+  var db = new sqlite3.Database("./db.sqlite", sqlite3.OPEN_READWRITE, err => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the sqLite database");
+    debugger;
+  });
+
+  let sourceRes = [];
+  db.serialize(() => {
+    db.each(
+      `SELECT *
+                 FROM source`,
+      (err, row) => {
+        if (err) {
+          console.error(err.message);
+        }
+        sourceRes.push(row);
+      }
+    );
+  });
   app.post("/source", (req, res) => {
-    res.send("Hello. There are some sources.");
+    res.send(sourceRes);
+  });
+
+  let messageRes = [];
+  db.serialize(() => {
+    db.each(
+      `SELECT *
+     FROM message`,
+      (err, row) => {
+        if (err) {
+          console.error(err.message);
+        }
+        messageRes.push(row);
+      }
+    );
   });
   app.post("/message", (req, res) => {
-    res.send("Hello. There are some messages.");
+    res.send(messageRes);
   });
 };
