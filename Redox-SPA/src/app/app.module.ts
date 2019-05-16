56374c81db50517dfcca5999033fee91a2ce7c68
K. H <knhg2013@gmail.com>
2019-05-15 23:51:07 -0400

    The source list component/page can get the data from real API, with the URI http://localhost:8888/source. Also, the CORS issus is fixed.

--- Redox-SPA/src/app/app.module.ts
+++ Redox-SPA/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { SingleSourceComponent } from "./single-source/single-source.component";
-import { SourcesComponent } from './sources/sources.component';
+import { SourcesComponent } from "./sources/sources.component";
 
 @NgModule({
   declarations: [AppComponent, SingleSourceComponent, SourcesComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- Redox-SPA/src/app/source.service.ts
+++ Redox-SPA/src/app/source.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
-//import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Observable, of } from "rxjs";
-import { catchError, map, tap } from "rxjs/operators";
+//import { catchError, map, tap } from "rxjs/operators";
 import { SOURCES } from "./mock-sources";
 import { Source } from "./source";
 
@@ -10,15 +10,16 @@ import { Source } from "./source";
   providedIn: "root"
 })
 export class SourceService {
-  //private sourcesUrl = "localhost:8888/source"; // URL to web api
+  private sourcesUrl = "http://localhost:8888/source"; // URL to web api
   sources = SOURCES;
-  //constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  //constructor() {}
 
   /** GET sourses data from the server */
   getSources(): Observable<Source[]> {
-    return of(this.sources);
+    //return of(this.sources);
+    return this.http.get<Source[]>(this.sourcesUrl);
     //return this.http.get<Source[]>(this.sourcesUrl).pipe();
   }
 }
