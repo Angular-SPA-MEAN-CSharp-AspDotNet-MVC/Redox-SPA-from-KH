23a02b426ab5d0e7acbd5ceac44a2b7c6e9bf0b3
K. H <knhg2013@gmail.com>
2019-05-15 22:59:41 -0400

    Get the soure list data from a service (prepare to reach a real API, I wrote)

--- Redox-SPA/src/app/source.service.ts
+++ Redox-SPA/src/app/source.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+//import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Observable, of } from "rxjs";
 import { catchError, map, tap } from "rxjs/operators";
-
+import { SOURCES } from "./mock-sources";
 import { Source } from "./source";
 
 @Injectable({
   providedIn: "root"
 })
 export class SourceService {
-  private sourcesUrl = "http://localhost:8888/source"; // URL to web api
+  //private sourcesUrl = "localhost:8888/source"; // URL to web api
+  sources = SOURCES;
+  //constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   /** GET sourses data from the server */
   getSources(): Observable<Source[]> {
-    return this.http.get<Source[]>(this.sourcesUrl).pipe();
+    return of(this.sources);
+    //return this.http.get<Source[]>(this.sourcesUrl).pipe();
   }
 }
--- Redox-SPA/src/app/sources/sources.component.ts
+++ Redox-SPA/src/app/sources/sources.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { SOURCES } from "../mock-sources";
-import { Source } from "webpack-sources";
+import { Source } from "../source";
+import { SourceService } from "../source.service";
 
 @Component({
   selector: "app-sources",
@@ -8,12 +9,22 @@ import { Source } from "webpack-sources";
   styleUrls: ["./sources.component.css"]
 })
 export class SourcesComponent implements OnInit {
-  sources = SOURCES;
+  //sources = SOURCES;
+  sources: Source[];
   selectedSource: Source;
 
-  constructor() {}
+  constructor(private sourceService: SourceService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getSources();
+  }
+
+  getSources(): void {
+    this.sourceService
+      .getSources()
+      .subscribe(sources => (this.sources = sources));
+    //this.sources = this.sourceService.getSources();
+  }
 
   onSelect(oneSource: Source): void {
     this.selectedSource = oneSource;
